bb322a67fffaeac89c4532d575dd43408a39ee32
danielsetoubu <dsr0018@alu.ubu.es>
2020-07-16 14:14:15 +0200

    #14 Add class TransientSequence.py
    A fixed size Sequence type for managing large in-memory structures.

---
+++ olivia/lib/TransientSequence.py
@@ -0,0 +1,145 @@
+import pickle
+import zlib
+from collections.abc import Sequence
+
+
+class TransientSequence(Sequence):
+    """
+    A fixed size Sequence type for managing large in-memory structures.
+
+    A TransientSequence is a fixed size linear array of arbitrary objects that are compressed/decompressed
+    on the fly and automatically deleted from memory after a certain number of accesses.
+
+    Objects must implement __len__(). Only simple accessors are supported (no slicing).
+
+    TransientSequence has been developed to efficiently manage large successor sets of graphs using bit vector
+    representations. It should be used with care in a general setting as implicit compression on accessors
+    could be dangerous.
+
+    Parameters
+    ----------
+    size: int
+        Amount of objects to be stored (fixed).
+    class_type: class, optional
+        Type of objects to be stored.
+    compressor: function, optional
+        Compressing function. Defaults to simple pickle/zlib compressor.
+    decompressor: function, optional
+        Decompressing function. Defaults to simple zlib/unpickle decompressor.
+    compression_threshold:  int, optional
+        Upper size (len) limit to keep objects uncompressed.
+    expiry_array: sequence or None, optional
+        Number of accesses per object before deletion. Reassigning reverts the counter to its original value.
+    """
+
+    @staticmethod
+    def DEFAULT_COMPRESSOR(value):
+        return zlib.compress(pickle.dumps(value))
+
+    @staticmethod
+    def DEFAULT_DECOMPRESSOR(value):
+        return pickle.loads(zlib.decompress(value))
+
+    def __init__(self, size, class_type=object, compressor=DEFAULT_COMPRESSOR.__func__,
+                 decompressor=DEFAULT_DECOMPRESSOR.__func__,
+                 compression_threshold=1000, expiry_array=None):
+        """Creates and initializes a TransientSequence.
+
+        Parameters
+        ----------
+        size: int
+            Amount of objects to be stored (fixed).
+        class_type: class, optional
+            Type of objects to be stored.
+        compressor: function, optional
+            Compressing function. Defaults to simple pickle/zlib compressor.
+        decompressor: function, optional
+            Decompressing function. Defaults to simple zlib/unpickle decompressor.
+        compression_threshold:  int, optional
+            Upper size (len) limit to keep objects uncompressed.
+        expiry_array: sequence or None, optional
+            Number of accesses per object before deletion. Reassigning reverts the counter to its original value.
+        """
+        self._data = [None] * size
+        self._compressed = [False] * size
+        self._ct = compression_threshold
+        self._compressor = compressor
+        self._decompressor = decompressor
+        self._class_type = class_type
+        self._expiry = expiry_array
+        if expiry_array is not None:
+            self._expiry_original = expiry_array.copy()
+
+    def __setitem__(self, index, value):
+        """Assigns value to position 'index'.
+
+        Parameters
+        ----------
+        index : int
+            A position in the TransientSequence.
+        value : object
+            An object or object of class_type.
+
+        Returns
+        -------
+        None
+
+        Notes
+        -----
+        Transparently compresses the object if length > compression_threshold.
+        """
+        if value is None:
+            self._data[index] = None
+            return
+        if len(value) == 0:
+            self._data[index] = None
+        if not isinstance(value, self._class_type):
+            value = self._class_type(value)
+        if len(value) > self._ct:
+            self._data[index] = self._compressor(value)
+            self._compressed[index] = True
+        else:
+            self._data[index] = value
+            self._compressed[index] = False
+        if self._expiry is not None:
+            self._expiry[index] = self._expiry_original[index]
+
+    def __getitem__(self, index):
+        """Returns the object at position 'index'.
+
+        Parameters
+        ----------
+        index : int
+            A position in the TransientSequence.
+
+        Returns
+        -------
+        object : object
+            Object or object of class_type.
+
+        Notes
+        -----
+        Transparently decompresses the object if needed and deletes the reference if compression_threshold
+        is exceeded.
+        """
+        if self._data[index] is None:
+            out = self._class_type()
+        elif self._compressed[index]:
+            out = self._decompressor(self._data[index])
+        else:
+            out = self._data[index]
+        if self._expiry is not None:
+            self._expiry[index] -= 1
+            if self._expiry[index] == 0:
+                self._data[index] = None
+        return out
+
+    def __len__(self):
+        """Returns the length of the TransientSequence.
+
+        Returns
+        -------
+        length : int
+            Number of objects in the TransientSequence (length)
+        """
+        return len(self._data)
---
+++ olivia/tests/lib/test_TransientSequence.py
@@ -0,0 +1,66 @@
+from olivia.lib.TransientSequence import TransientSequence
+
+
+def test_create_simple():
+    size = 10
+    s = TransientSequence(size)
+    assert len(s) == size, 'Wrong size'
+    for n in range(size):
+        assert isinstance(s[n], object), 'Not initialized value in new TransientSequence'
+    # list
+    for n in range(size):
+        s[n] = [s for s in range(n)]
+    for n in range(size):
+        assert s[n] == [s for s in range(n)], 'Diff. in simple lists'
+    # set
+    for n in range(size):
+        s[n] = {s for s in range(n)}
+    for n in range(size):
+        assert s[n] == {s for s in range(n)}, 'Diff in simple sets'
+
+
+def test_create_typed():
+    s = TransientSequence(2, class_type=set)
+    s[0] = None
+    assert isinstance(s[0], set), 'Empty object not created from None assignment'
+    s[0] = [1, 2, 3]
+    assert isinstance(s[0], set), 'Wrong object type'
+    assert s[0] == {1, 2, 3}, 'Object not matching'
+
+
+def test_compress_dict():
+    threshold = 10
+    s = TransientSequence(2, class_type=dict,
+                          compression_threshold=threshold, expiry_array=None)
+    d0 = {n: n + 1 for n in range(threshold)}  # not compressed
+    d1 = {n: n + 1 for n in range(threshold + 1)}  # compressed
+    s[0] = d0
+    s[1] = d1
+    assert s[0] == d0, 'Non matching dicts'
+    assert s[1] == d1, 'Non matching dicts'
+    # a quirky access to internal list to check compression
+    assert s._data[0] == d0, 'Non matching dict'
+    assert s._data[1] != d1, 'Dict not compressed'
+
+
+def test_expiry_compress_set():
+    threshold = 2
+    s = TransientSequence(10, class_type=set,
+                          compression_threshold=threshold, expiry_array=[1, 2])
+    s[0] = {1, 2, 3, 4}
+    s[1] = {1, 2, 3}
+    assert s[0] == {1, 2, 3, 4}, 'Non matching set'  # live
+    assert len(s[0]) == 0, 'Not expired'  # expired
+    assert s[1] == {1, 2, 3}, 'Non matching set'  # live
+    assert s[1] == {1, 2, 3}, 'Non matching set'  # live
+    assert len(s[1]) == 0, 'Not expired'  # expired
+
+
+def test_custom_compressor():
+
+    s = TransientSequence(10, class_type=list, compressor=lambda i: ''.join(i),
+                          decompressor=lambda o: list([*o]), compression_threshold=1)
+    test_list = ['a', 'b', 'c', 'd']
+    s[0] = test_list
+    assert s._data[0] != test_list, 'Not compressed'
+    assert s[0] == test_list, 'Non matching decompressed get_item'
